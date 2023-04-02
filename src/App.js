715da62cc825f687153a09712614ab07e2369939
cherrybytedev <89452621+walifile@users.noreply.github.com>
2023-04-03 00:29:56 +0500

    feat : filter options for brightness, saturation, inversion, and grayscale.

--- src/App.js
+++ src/App.js
@@ -1,6 +1,12 @@
 import React, { useState, useRef, useEffect } from "react";
 import "./App.css";
 
+const filterOptions = [
+  { id: "brightness", name: "Brightness" },
+  { id: "saturation", name: "Saturation" },
+  { id: "inversion", name: "Inversion" },
+  { id: "grayscale", name: "Grayscale" },
+];
 function App() {
   const [previewImg, setPreviewImg] = useState(null);
   const [brightness, setBrightness] = useState("100");
@@ -13,16 +19,20 @@ function App() {
   const [flipVertical, setFlipVertical] = useState(1);
   const [selectedFilter, setSelectedFilter] = useState("brightness");
   const [sliderMax, setSliderMax] = useState(null);
-  const [sliderValue, setSliderValue] = useState(null);
   const [filterValue, setFilterValue] = useState(null);
-  const [activeFilter, setActiveFilter] = useState(null);
   const fileInputRef = useRef(null);
   const previewImgRef = useRef(null);
   const filterNameRef = useRef(null);
   const filterValueRef = useRef(null);
   const resetFilterBtnRef = useRef(null);
+  const [activeFilter, setActiveFilter] = useState("brightness");
+  const [sliderValue, setSliderValue] = useState(100);
+  const [filterType, setFilterType] = useState("brightness");
+  const [filterName, setFilterName] = useState("");
+
   function handleFilterClick(filter) {
     setActiveFilter(filter);
+    setSelectedFilter(filter);
     // perform filtering logic here
   }
   const loadImage = (e) => {
@@ -40,23 +50,9 @@ function App() {
       // setDisable(false);
     }
   }, [previewImg]);
-  const applyFilter = () => {
-    previewImgRef.current.style.transform = `rotate(${rotate}deg) scale(${flipHorizontal}, ${flipVertical})`;
-    previewImgRef.current.style.filter = `brightness(${brightness}%) saturate(${saturation}%) invert(${inversion}%) grayscale(${grayscale}%)`;
-  };
 
-  const updateFilter = (e) => {
-    const value = e.target.value;
-    if (selectedFilter === "brightness") {
-      setBrightness(value);
-    } else if (selectedFilter === "saturation") {
-      setSaturation(value);
-    } else if (selectedFilter === "inversion") {
-      setInversion(value);
-    } else {
-      setGrayscale(value);
-    }
-    applyFilter();
+  const applyFilter = () => {
+    previewImgRef.current.style.filter = `brightness(${brightness}%) saturate(${saturation}%) invert(${inversion}%) grayscale(${grayscale}%)`;
   };
 
   const resetFilter = () => {
@@ -102,23 +98,44 @@ function App() {
     image.src = URL.createObjectURL(previewImg);
   };
 
-  // const handleFilterClick = (e) => {
-  //   const id = e.target.id;
-  //   setSelectedFilter(id);
-  //   if (id === "brightness") {
-  //     setSliderMax("200");
-  //     setSliderValue(brightness);
-  //     setFilterValue(`${brightness}%`);
-  //   } else if (id === "saturation") {
-  //     setSliderMax("200");
-  //     setSliderValue(saturation);
-  //     setFilterValue(`${saturation}%`);
-  //   } else if (id === "inversion") {
-  //     setSliderMax("100");
-  //     setSliderValue(inversion);
-  //     // setFilterValue(`${in
-  //   }
-  // };
+  function handleFilterClick(option) {
+    setActiveFilter(option.id);
+    setFilterName(option.name);
+
+    switch (option.id) {
+      case "brightness":
+        setSliderValue(brightness);
+        setFilterType("brightness");
+        break;
+      case "saturation":
+        setSliderValue(saturation);
+        setFilterType("saturation");
+        break;
+      case "inversion":
+        setSliderValue(inversion);
+        setFilterType("inversion");
+        break;
+      default:
+        setSliderValue(grayscale);
+        setFilterType("grayscale");
+    }
+  }
+  function handleSliderChange(event) {
+    setSliderValue(event.target.value);
+    switch (filterType) {
+      case "brightness":
+        setBrightness(event.target.value);
+        break;
+      case "saturation":
+        setSaturation(event.target.value);
+        break;
+      case "inversion":
+        setInversion(event.target.value);
+        break;
+      default:
+        setGrayscale(event.target.value);
+    }
+  }
   return (
     <div className="container disable">
       <h2>Easy Image Editor</h2>
@@ -126,43 +143,39 @@ function App() {
         <div className="editor-panel">
           <div className="filter">
             <label className="title">Filters</label>
+
             <div className="options">
-              <button
-                id="brightness"
-                className="active"
-                onClick={() => handleFilterClick("brightness")}
-              >
-                Brightness
-              </button>
-              <button
-                id="saturation"
-                onClick={() => handleFilterClick("saturation")}
-              >
-                Saturation
-              </button>
-              <button
-                id="inversion"
-                onClick={() => handleFilterClick("inversion")}
-              >
-                Inversion
-              </button>
-              <button
-                id="grayscale"
-                onClick={() => handleFilterClick("grayscale")}
-              >
-                Grayscale
-              </button>
+              {filterOptions.map((option) => (
+                <button
+                  key={option.id}
+                  id={option.id}
+                  className={activeFilter === option.id ? "active" : ""}
+                  onClick={() => handleFilterClick(option)}
+                >
+                  {option.name}
+                </button>
+              ))}
             </div>
             <div className="slider">
               <div className="filter-info">
                 <p className="name" ref={filterNameRef}>
-                  Brighteness
+                  {filterName}
                 </p>
                 <p className="value" ref={filterValueRef}>
-                  100%
+                  {`${sliderValue}%`}
                 </p>
               </div>
-              <input type="range" value="100" min="0" max="200" />
+              <input
+                type="range"
+                min="0"
+                max={
+                  filterType === "brightness" || filterType === "saturation"
+                    ? "200"
+                    : "100"
+                }
+                value={sliderValue}
+                onChange={handleSliderChange}
+              />
             </div>
           </div>
           <div className="rotate">
@@ -185,13 +198,14 @@ function App() {
         </div>
         <div className="preview-img">
           {previewImg ? (
-            <img src={URL.createObjectURL(previewImg)} alt="preview" />
-          ) : (
             <img
-              src="image-placeholder.svg"
-              alt="preview-img"
+              src={URL.createObjectURL(previewImg)}
+              alt="preview"
               ref={previewImgRef}
+              onLoad={applyFilter}
             />
+          ) : (
+            <img src="image-placeholder.svg" alt="preview-img" />
           )}
         </div>
       </div>
@@ -214,7 +228,9 @@ function App() {
           >
             Choose Image
           </button>
-          <button onClick={saveImage}>Save Image</button>
+          <button onClick={saveImage} className="save-img">
+            Save Image
+          </button>
         </div>
       </div>
     </div>
